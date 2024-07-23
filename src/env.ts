f50766af6ca4309ce2f35af9f9c57c78333feaca
Damir Modyarov <otomir@yandex.ru>
2024-07-23 16:53:12 +0300

    fix: Use proper number types for env variables

--- src/env.ts
+++ src/env.ts
@@ -5,11 +5,11 @@ import { z } from 'zod'
 
 export const env = createEnv({
     server: {
-        API_ID: z.number().int(),
+        API_ID: z.coerce.number().int(),
         API_HASH: z.string(),
         LASTFM_KEY: z.string(),
         LASTFM_USERNAME: z.string(),
-        UPDATE_INTERVAL_SECONDS: z.number().int().default(10),
+        UPDATE_INTERVAL_SECONDS: z.coerce.number().int().default(10),
         FALLBACK_MESSAGE: z.string().default('Nothing playing!'),
     },
     runtimeEnv: process.env,
